cbfbfcabc89562a771782954172a02c9f587ea63
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-03 16:47:11 +0600

    Created HelloWorld program

---
+++ helloworld/src/com/auca/Main.java
@@ -0,0 +1,16 @@
+package com.auca;
+
+/**
+ * @author Yiman
+ *
+ * Simple HelloWorld program in Java
+ *
+ * */
+// Main
+public class Main {
+
+    public static void main(String[] args) {
+	    // prints Hello World!
+        System.out.println("Hello World!");
+    }
+}
