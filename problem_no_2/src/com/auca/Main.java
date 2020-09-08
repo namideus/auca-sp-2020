32f65c99e0fc36ddab3cb8d9d005cec7c012e16b
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-08 21:05:43 +0600

    Solved problem #2

---
+++ problem_no_2/src/com/auca/Main.java
@@ -0,0 +1,17 @@
+package com.auca;
+
+/**
+ * @author Yiman
+ *
+ * "A Message in a Rectangle"
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*I like programming!*");
+        System.out.println("*********************");
+    }
+}
