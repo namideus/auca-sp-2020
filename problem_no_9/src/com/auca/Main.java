e83abe1a1a6b1b8e5e68f8aeaf454f0caab632cd
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-09 17:49:27 +0600

    Solved problem #9

---
+++ problem_no_9/src/com/auca/Main.java
@@ -0,0 +1,34 @@
+package com.auca;
+
+import java.util.Scanner;
+
+/**
+ * @author Yiman
+ *
+ * Approximate the number PI.
+ *
+ * */
+
+public class Main
+{
+    // Main
+    public static void main(String[] args)
+    {
+        System.out.println("PI = " + PI(6)); // n = 6
+        System.out.println("PI = " + PI(7)); // N = 7
+    }
+
+    // Approximates the constant PI for arbitrary N value
+    public static double PI(int n) {
+
+        double sum = 0;
+        int denom = 1;
+
+        for (int i=1; i<=n; i++)
+        {
+            sum += Math.pow(-1,(i+1))*(1.0/denom);
+            denom += 2;
+        }
+        return 4*sum;
+    }
+}
