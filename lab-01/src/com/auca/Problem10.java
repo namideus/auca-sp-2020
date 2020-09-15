034dcb68d39ab64c69ab8c177b0ba3c4d30b6df0
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-15 15:20:06 +0600

    Solved problem #10 from lab-01

--- lab-01/src/com/auca/Problem10.java
+++ lab-01/src/com/auca/Problem10.java
@@ -3,14 +3,32 @@ package com.auca;
 /**
  * @author Yiman
  *
+ * "Population projection" program
  *
  * */
 
 public class Problem10 {
 
+    // Global variables
+    private static long totalPopulation = 312032486;
+    private static final long yearDays = 365;
+
     // Main
     public static void main(String[] args) {
 
+        int n = 1;
+        long yearSeconds = yearDays*24*60*60; // Total seconds in one year
+
+        for(int period = 0; period < n; period++) {
+            for (int year = 0; year < 5; year++) {
+                totalPopulation += yearSeconds / 7.0;  // One birth every 7 seconds
+                totalPopulation -= yearSeconds / 13.0; // One death every 13 seconds
+                totalPopulation += yearSeconds / 45.0; // One new immigrant every 45 seconds
+            }
+        }
+
+        // Print updated total population of the United States
+        System.out.println("Population after " + (n*5) + " years: " + totalPopulation);
     }
 
 }
