5c11b361d14bc9ed2e6e3cc4f50eb71931f892a0
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-16 22:21:18 +0600

    URI p-1001 accepted

---
+++ URI_midterm/p-1001/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.printf("X = %d\n", a+b);
+    }
+}
