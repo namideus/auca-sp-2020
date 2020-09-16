7d62a1f7b64c96771cdaafba7c46b122bd253011
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-16 22:59:53 +0600

    URI p-1003 accepted

---
+++ URI_midterm/p-1003/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int SOMA = A + B;
+        System.out.printf("SOMA = %d\n", SOMA);
+    }
+}
