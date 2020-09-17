2d122ef7146465344b15b3bdd48adf6e8452258d
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-17 15:54:15 +0600

    URI p-1005 accepted

--- URI_midterm/p-1005/src/Main.java
+++ URI_midterm/p-1005/src/Main.java
@@ -8,10 +8,9 @@ import java.util.Scanner;
 public class Main {
     // Main
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double A = scan.nextDouble();
-        double B = scan.nextDouble();
-        double MEDIA = (A*3.5 + B*7.5)/2;
-        System.out.printf("MEDIA = %.5f\n", MEDIA);
+        Scanner s = new Scanner(System.in); // Scanner
+        float a = s.nextFloat(); // A grade
+        float b = s.nextFloat(); // B grade
+        System.out.printf("MEDIA = %.5f\n", ((a*3.5) + (b*7.5))/11 ); // Print average grade
     }
 }
