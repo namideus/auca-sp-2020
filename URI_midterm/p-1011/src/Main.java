8840f470ad704cbb83493391f0e28f3c03418c91
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-17 16:27:07 +0600

    URI p-1011

---
+++ URI_midterm/p-1011/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    private static final double PI = 3.14159;
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int R = s.nextInt();
+        double V = (4.0/3)*PI*R*R*R;
+        System.out.printf("VOLUME = %.3f\n", V);
+    }
+}
