5f5d1ac6b10ee3d26d81bc7dcb99aaa4e7cf70a7
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-17 16:02:48 +0600

    URI p-1007 accepted

---
+++ URI_midterm/p-1007/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int d = s.nextInt();
+        System.out.printf("DIFERENCA = %d\n", (a*b-c*d) );
+    }
+}
