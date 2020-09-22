0219460fab61b9b5e15c6b7e23a081d390623433
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-23 00:46:21 +0600

    URI p-1016 accepted

---
+++ URI_midterm/p-1016/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int dist = s.nextInt();
+        System.out.printf("%d minutos\n", dist*2);
+    }
+}
