19b028037c1d841c8171d8bfaf0ac128d1973cd5
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-23 20:04:06 +0600

    Solved problem #2

---
+++ lab-02/src/com/auca/Problem02.java
@@ -0,0 +1,24 @@
+package com.auca;
+
+import java.util.Scanner;
+
+/**
+ * @author Yiman
+ *
+ * */
+
+public class Problem02 {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+	    System.out.print("1st value? ");
+	    int a = scan.nextInt();
+        System.out.print("2nd value? ");
+        int b = scan.nextInt();
+        System.out.printf("Before swapping: a = %d; b = %d\n", a, b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.printf("After swapping: a = %d; b = %d\n", a, b);
+    }
+}
