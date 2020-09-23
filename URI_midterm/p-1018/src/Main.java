91964f6a68ae1c84707a416f21cd5e63925185cf
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-23 12:45:12 +0600

    URI p-1018 presentation error

--- URI_midterm/p-1018/src/Main.java
+++ URI_midterm/p-1018/src/Main.java
@@ -9,8 +9,20 @@ public class Main {
     // Main
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int time = s.nextInt();
-        int speed = s.nextInt();
-        System.out.printf("%.3f\n", (time*speed)/12.0);
+        int n = s.nextInt();
+        System.out.printf("%d\n", n);
+        System.out.printf("%d nota(s) de R$ 100,00\n", n/100);
+        n%=100;
+        System.out.printf("%d nota(s) de R$ 50,00\n", n/50);
+        n%=50;
+        System.out.printf("%d nota(s) de R$ 20,00\n", n/20);
+        n%=20;
+        System.out.printf("%d nota(s) de R$ 10,00\n", n/10);
+        n%=10;
+        System.out.printf("%d nota(s) de R$ 5,00\n", n/5);
+        n%=5;
+        System.out.printf("%d nota(s) de R$ 2,00\n", n/2);
+        n%=2;
+        System.out.printf("%d nota(s) de R$ 1,00", n);
     }
 }
