89b2a06adc6f46a0ade62f01d8c6f6f6f6681c2b
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-23 20:52:48 +0600

    Solved problem #5 lab-02

---
+++ lab-02/src/com/auca/Problem05.java
@@ -0,0 +1,19 @@
+package com.auca;
+
+import java.util.Scanner;
+
+/**
+ * @author Yiman
+ *
+ * */
+
+public class Problem05 {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Temperature in degrees Fahrenheit? ");
+        int fahrenheit = scan.nextInt();
+        double celsius = (fahrenheit-32)*(5/9.0);
+        System.out.printf("The temperature in degrees Celsius is %.2f\n", celsius);
+    }
+}
