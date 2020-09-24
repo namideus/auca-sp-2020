1bd29d270828f0bb2ef090579036a316c5cdc44e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-24 14:41:03 +0600

    URI p-1074 accepted

---
+++ URI_midterm/p-1074/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i ++)
+        {
+            int dig = scan.nextInt();
+
+            if(dig==0)
+            {
+                System.out.print("NULL\n");
+                continue;
+            }
+
+            if(dig%2==0)
+                System.out.print("EVEN ");
+            else
+                System.out.print("ODD ");
+
+            if(dig>0)
+                System.out.print("POSITIVE\n");
+            else if(dig<0)
+                System.out.print("NEGATIVE\n");
+        }
+    }
+}
