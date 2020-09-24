92d4790e949ef1235e51a18f32665456dda805ec
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-24 15:52:51 +0600

    Solved problem #10 lab-02 but 4 is the difference between my result and the result in the lab-02 task sheet

---
+++ lab-02/src/com/auca/Problem10.java
@@ -0,0 +1,33 @@
+package com.auca;
+
+import java.util.Scanner;
+
+/**
+ * @author Yiman
+ *
+ * */
+
+public class Problem10 {
+
+    // Global variables
+    private static long totalPopulation = 312032486;
+    private static final long yearDays = 365;
+
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number of years: ");
+        int n = scan.nextInt();
+
+        long yearSeconds = yearDays * 24 * 60 * 60; // Total seconds in one year
+
+        for (int year = 0; year < n; year++) {
+            totalPopulation += yearSeconds / 7;  // One birth every 7 seconds
+            totalPopulation -= yearSeconds / 13; // One death every 13 seconds
+            totalPopulation += yearSeconds / 45; // One new immigrant every 45 seconds
+        }
+        // Print updated total population of the United States
+        System.out.printf("The population in %d years is %d\n", n, totalPopulation);
+    }
+}
