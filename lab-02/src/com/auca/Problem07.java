4dad9206e152ba828ad78173dbf7f6192045e7f3
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-24 15:11:42 +0600

    Solved problem #7 lab-02

---
+++ lab-02/src/com/auca/Problem07.java
@@ -0,0 +1,21 @@
+package com.auca;
+
+import java.util.Scanner;
+
+/**
+ * @author Yiman
+ *
+ * */
+
+public class Problem07 {
+    // Main
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the radius and length of a cylinder: ");
+        double radius = scan.nextDouble();
+        int length = scan.nextInt();
+        double area = radius*radius*Math.PI;
+        double volume = area*length;
+        System.out.printf("The area is %.4f\nThe volume is %.1f\n", area, volume);
+    }
+}
