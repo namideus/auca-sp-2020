81c485b6024b66b55d63f7b83bd817594142f25e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-27 15:37:29 +0600

    URI p-1036 wrong answer 15%

--- URI_midterm/p-1036/src/Main.java
+++ URI_midterm/p-1036/src/Main.java
@@ -10,19 +10,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        double a = s.nextDouble();
-        double b = s.nextDouble();
-        double c = s.nextDouble();
+        double a = s.nextFloat();
+        double b = s.nextFloat();
+        double c = s.nextFloat();
         double x1, x2, delta;
-        delta = b * b - 4 * a * c;
+        delta = (b * b) - (4 * a * c);
 
-        if( a!=0 && delta>=0) {
-            x1 = (-b + Math.sqrt(delta))/2*a;
-            x2 = (-b -Math.sqrt(delta))/2*a;
-            System.out.printf("R1 = %.5f\n", x1/100.0);
-            System.out.printf("R2 = %.5f\n", x2/100.0);
-        } else {
+        if( a==0 || delta<0) {
             System.out.println("Impossivel calcular");
+        } else {
+            x1 = (-b + Math.sqrt(delta))/2*a;
+            x2 = (-b - Math.sqrt(delta))/2*a;
+            System.out.printf("R1 = %.5f\n", x1);
+            System.out.printf("R2 = %.5f\n", x2);
         }
     }
 }
