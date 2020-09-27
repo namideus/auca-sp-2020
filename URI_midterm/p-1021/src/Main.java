ca7650effe84608c9a241535ef4da090193341fd
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-09-27 14:57:29 +0600

    URI p-1021 wrong answer %5

--- URI_midterm/p-1021/src/Main.java
+++ URI_midterm/p-1021/src/Main.java
@@ -13,18 +13,18 @@ public class Main {
         float n = s.nextFloat();
 
         System.out.print("NOTAS:\n");
-        System.out.printf("%d nota(s) de R$ 100.00\n", (int)(n/100));
-        n%=100;
-        System.out.printf("%d nota(s) de R$ 50.00\n", (int)(n/50));
-        n%=50;
-        System.out.printf("%d nota(s) de R$ 20.00\n", (int)(n/20));
-        n%=20;
-        System.out.printf("%d nota(s) de R$ 10.00\n", (int)(n/10));
-        n%=10;
-        System.out.printf("%d nota(s) de R$ 5.00\n", (int)(n/5));
-        n%=5;
-        System.out.printf("%d nota(s) de R$ 2.00\n", (int)(n/2));
-        n%=2;
+        System.out.printf("%d nota(s) de R$ 100.00\n", (int)(n/100.00));
+        n%=100.00;
+        System.out.printf("%d nota(s) de R$ 50.00\n", (int)(n/50.00));
+        n%=50.00;
+        System.out.printf("%d nota(s) de R$ 20.00\n", (int)(n/20.00));
+        n%=20.00;
+        System.out.printf("%d nota(s) de R$ 10.00\n", (int)(n/10.00));
+        n%=10.00;
+        System.out.printf("%d nota(s) de R$ 5.00\n", (int)(n/5.00));
+        n%=5.00;
+        System.out.printf("%d nota(s) de R$ 2.00\n", (int)(n/2.00));
+        n%=2.00;
 
         System.out.print("MOEDAS:\n");
         System.out.printf("%d moeda(s) de R$ 1.00\n", (int)(n/1.00));
