e90092d086c3f6106ac0eb8dc2f7b3b4a466acbc
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-02 13:39:12 +0600

    URI p-1052 accepted

--- URI_midterm/p-1052/src/Main.java
+++ URI_midterm/p-1052/src/Main.java
@@ -9,9 +9,10 @@ public class Main {
     // Main
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        double salary = scan.nextDouble();
-
-
+        String months[] = new String[]{"January", "February", "March", "April",
+                                        "May", "June", "July","August","September",
+                                        "October","November","December"};
+        int month = scan.nextInt();
+        System.out.println(months[month-1]);
     }
 }
