c298ffd807a7c60078df87c41acc673878a05405
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-02 15:27:41 +0600

    URI p-1096 accepted

--- URI_midterm/p-1096/src/Main.java
+++ URI_midterm/p-1096/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        for(int i=1;i<=9;i++)
+        for(int i=1;i<=9;i+=2)
         {
             for(int j=7;j>=5;j--)
                 System.out.printf("I=%d J=%d\n", i, j);
