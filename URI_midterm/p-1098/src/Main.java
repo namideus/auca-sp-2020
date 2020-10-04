675ed2acd6f5afa18c672b8b16924dcee17366f2
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-04 11:25:39 +0600

    URI p-1099 accepted

--- URI_midterm/p-1098/src/Main.java
+++ URI_midterm/p-1098/src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 /**
@@ -8,15 +9,15 @@ import java.util.Scanner;
 public class Main {
     // Main
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
 
         for(double i=0; i<=2; i+=0.2)
         {
-            for(int j=1;j<=3;j++)
-               if(i%1>0.0)
-                   System.out.printf("I=%.1f J=%.1f\n", i, j+i);
-               else
-                   System.out.printf("I=%d J=%d\n", (int)i, (int)(j+i));
+            for(int j=1;j<=3;j++) {
+                if ((i % 1) > 0)
+                    System.out.printf("I=%.1f J=%.1f\n", i, i + j);
+                else
+                    System.out.printf("I=%d J=%d\n", (int) i, (int) (j + i));
+            }
         }
     }
 }
---
+++ URI_midterm/p-1099/src/Main.java
@@ -0,0 +1,34 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,t,sum,n = sc.nextInt();
+
+        while(n>0)
+        {
+            sum=0;
+
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if(a>b) {
+                t = a;
+                a = b;
+                b = t;
+            }
+            for(int d=a+1; d<b;d++)
+            {
+                sum += (d%2>0) ? d : 0;
+            }
+            System.out.println(sum);
+            n--;
+        }
+    }
+}
