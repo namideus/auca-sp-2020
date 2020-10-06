1739084470d24eb834ff7e5c864b49486df82c3d
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-06 19:26:42 +0600

    URI p-1101 wrong answer 15%

---
+++ URI_midterm/p-1101/src/Main.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m,n,sum;
+        m = sc.nextInt();
+        n = sc.nextInt();
+
+        while(m>0 && n>0)
+        {
+            sum = 0;
+            for(int i=n;i<=m;i++) {
+                System.out.printf("%d ", i);
+                sum+=i;
+            }
+            System.out.printf("Sum=%d\n", sum);
+
+            m = sc.nextInt();
+            n = sc.nextInt();
+        }
+    }
+}
