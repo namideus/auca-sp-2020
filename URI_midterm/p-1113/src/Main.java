dbee84ec2058fa65cdb60c6ef012ebe397e73c0f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-06 19:34:10 +0600

    URI p-1113 accepted

---
+++ URI_midterm/p-1113/src/Main.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m,n,sum;
+        m = sc.nextInt();
+        n = sc.nextInt();
+
+        while(m!=n)
+        {
+            sum = 0;
+
+            if(m<n) System.out.println("Crescente");
+
+            if(m>n) System.out.println("Decrescente");
+
+            m = sc.nextInt();
+            n = sc.nextInt();
+        }
+    }
+}
