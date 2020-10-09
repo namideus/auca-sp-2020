ea9a608b47d577c1cab2e85d1ff08fee447c16c2
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 20:25:35 +0600

    Task #4 #5 #6 solved from lab-03

--- lab-03/src/com/auca/Problem06.java
+++ lab-03/src/com/auca/Problem06.java
@@ -1,5 +1,7 @@
 package com.auca;
 
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
@@ -8,6 +10,25 @@ package com.auca;
 public class Problem06 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a, b, c: ");
+        double a,b,c, disc,x1,x2;
+        a = s.nextDouble();
+        b = s.nextDouble();
+        c = s.nextDouble();
+
+        disc = b*b - 4*a*c;
+
+        x1 = (-b+Math.sqrt(disc))/2*a;
+        x2 = (-b-Math.sqrt(disc))/2*a;
+
+        if (disc>0)
+            System.out.printf("The equation has two roots %g and %g\n", x1, x2);
+
+        if (disc==0)
+            System.out.printf("The equation has one root %g\n", x1);
+
+        if(disc<0)
+            System.out.print("The equation has no real roots\n");
     }
 }
