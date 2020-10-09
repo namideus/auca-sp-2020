5ee0416bae46245dc79c138b6af19344a705537d
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 07:17:21 +0600

    URI p-1134 presentation error

---
+++ URI_midterm/p-1134/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inter = 0, x, alc = 0, gas = 0, dies = 0;
+
+        while (true)
+        {
+            x = sc.nextInt();
+
+            if(x==1)
+                alc++;
+
+            if(x==2)
+                gas++;
+
+            if(x==3)
+                dies++;
+
+            if(x==4)
+                break;
+        }
+        System.out.println("MUITO OBRIGADO");
+        System.out.printf("Alcool: %d\n", alc);
+        System.out.printf("Gasolina: %d\n", gas);
+        System.out.printf("Diesel:%d\n", dies);
+    }
+}
