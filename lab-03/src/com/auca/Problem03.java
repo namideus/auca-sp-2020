6e04e039ae8a30df7951d8cde43eec63c949e6eb
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 08:18:24 +0600

    Task #3 solved from lab-03

--- lab-03/src/com/auca/Problem03.java
+++ lab-03/src/com/auca/Problem03.java
@@ -1,5 +1,7 @@
 package com.auca;
 
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
@@ -8,6 +10,10 @@ package com.auca;
 public class Problem03 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("1st number? "); int a = s.nextInt();
+        System.out.print("2nd number? "); int b = s.nextInt();
+        System.out.print("3rd number? "); int c = s.nextInt();
+        System.out.printf("The value %d is the greatest one.\n", Math.max(a, Math.max(b,c)));
     }
 }
