96a1d4183788166ffe220906955d301658e7dac6
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 21:26:27 +0600

    Task #10 solved from lab-03

--- lab-03/src/com/auca/Problem10.java
+++ lab-03/src/com/auca/Problem10.java
@@ -1,13 +1,25 @@
 package com.auca;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
  * */
 
 public class Problem10 {
-
+    private static final String[] srp = {"scissor", "rock", "paper"};
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a point with two coordinates: ");
+        double x,y;
+        x = s.nextDouble();
+        y = s.nextDouble();
+
+        if(Math.sqrt(x*x+y*y)<=10)
+            System.out.printf("Point (%.1f, %.1f) is in the circle\n", x,y);
+        else
+            System.out.printf("Point (%.1f, %.1f) is not in the circle\n", x,y);
     }
 }
