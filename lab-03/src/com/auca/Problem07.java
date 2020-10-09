1e09f20b5a8f8ec193b1184cd573837bf38d82d9
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 20:44:39 +0600

    Task #7 solved from lab-03

--- lab-03/src/com/auca/Problem07.java
+++ lab-03/src/com/auca/Problem07.java
@@ -1,5 +1,7 @@
 package com.auca;
 
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
@@ -7,7 +9,25 @@ package com.auca;
 
 public class Problem07 {
 
+    static final String[] weekDays = new String[] {
+            "Sunday","Monday","Tuesday","Wednesday",
+            "Thursday", "Friday","Saturday"
+    };
+
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        int today, futureDay;
+        System.out.print("Enter today's day: ");
+        today = s.nextInt();
+        System.out.print("Enter the number of days elapsed since today: ");
+        futureDay = s.nextInt();
+
+        if(futureDay<7)
+            futureDay+=today;
+
+        if(futureDay>7)
+            futureDay = (futureDay+today)%7;
+
+        System.out.printf("Today is %s and the future day is %s\n", weekDays[today], weekDays[futureDay]);
     }
 }
