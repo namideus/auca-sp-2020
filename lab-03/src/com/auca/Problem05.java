5d9edacfca8dbc466604f9ae1d54b143d85dc202
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 13:25:41 +0600

    Task #5 solved from lab-03

--- lab-03/src/com/auca/Problem05.java
+++ lab-03/src/com/auca/Problem05.java
@@ -1,5 +1,7 @@
 package com.auca;
 
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
@@ -8,6 +10,18 @@ package com.auca;
 public class Problem05 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("Number of points? ");
+        int grade = s.nextInt();
+        if(grade>=90 && grade<=100)
+            System.out.println("Grade: A");
+        else if(grade>=80 && grade<90)
+            System.out.println("Grade: B");
+        else if(grade>=70 && grade<80)
+            System.out.println("Grade: C");
+        else if(grade>=60 && grade<70)
+            System.out.println("Grade: D");
+        else
+            System.out.printf("%d is not in the permitted range.\n", grade);
     }
 }
