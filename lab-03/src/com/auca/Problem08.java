4c638d689668b5bf96740e1ef724bbff0e58ae3f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-09 20:51:19 +0600

    Task #8 solved from lab-03

--- lab-03/src/com/auca/Problem08.java
+++ lab-03/src/com/auca/Problem08.java
@@ -1,5 +1,8 @@
 package com.auca;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author Iman
  *
@@ -8,6 +11,12 @@ package com.auca;
 public class Problem08 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        int[] arr = new int[3];
+        arr[0] = s.nextInt();
+        arr[1] = s.nextInt();
+        arr[2] = s.nextInt();
+        Arrays.sort(arr);
+        System.out.printf("%d %d %d\n", arr[0],arr[1],arr[2]);
     }
 }
