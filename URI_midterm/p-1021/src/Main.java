4e26e7dd12be9088b1a7919316b74005515492f5
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-11 15:50:51 +0600

    URI p-1021 accepted

--- URI_midterm/p-1021/src/Main.java
+++ URI_midterm/p-1021/src/Main.java
@@ -11,32 +11,36 @@ public class Main {
         Scanner s = new Scanner(System.in);
 
         double n = s.nextDouble();
+        int notas = (int) n;
+        int moedas = (int) ((n % 1)*100);
+//        System.out.println(notas);
+//        System.out.println(moedas);
 
         System.out.print("NOTAS:\n");
-        System.out.printf("%d nota(s) de R$ 100.00\n", (int)(n/100.00));
-        n%=100;
-        System.out.printf("%d nota(s) de R$ 50.00\n", (int)(n/50.00));
-        n%=50;
-        System.out.printf("%d nota(s) de R$ 20.00\n", (int)(n/20.00));
-        n%=20;
-        System.out.printf("%d nota(s) de R$ 10.00\n", (int)(n/10.00));
-        n%=10;
-        System.out.printf("%d nota(s) de R$ 5.00\n", (int)(n/5.00));
-        n%=5;
-        System.out.printf("%d nota(s) de R$ 2.00\n", (int)(n/2.00));
-        n%=2;
+        System.out.printf("%d nota(s) de R$ 100.00\n", notas/100);
+        notas%=100;
+        System.out.printf("%d nota(s) de R$ 50.00\n", notas/50);
+        notas%=50;
+        System.out.printf("%d nota(s) de R$ 20.00\n", notas/20);
+        notas%=20;
+        System.out.printf("%d nota(s) de R$ 10.00\n", notas/10);
+        notas%=10;
+        System.out.printf("%d nota(s) de R$ 5.00\n", notas/5);
+        notas%=5;
+        System.out.printf("%d nota(s) de R$ 2.00\n", notas/2);
+        notas%=2;
 
         System.out.print("MOEDAS:\n");
-        System.out.printf("%d moeda(s) de R$ 1.00\n", (int)(n/1.00));
-        n%=1;
-        System.out.printf("%d moeda(s) de R$ 0.50\n", (int)(n/0.5));
-        n%=0.50;
-        System.out.printf("%d moeda(s) de R$ 0.25\n", (int)(n/0.25));
-        n%=0.25;
-        System.out.printf("%d moeda(s) de R$ 0.10\n", (int)(n/0.10));
-        n%=0.10;
-        System.out.printf("%d moeda(s) de R$ 0.05\n", (int)(n/0.05));
-        n%=0.05;
-        System.out.printf("%d moeda(s) de R$ 0.01\n", (int)(n/0.01));
+        System.out.printf("%d moeda(s) de R$ 1.00\n", notas);
+        moedas%=100;
+        System.out.printf("%d moeda(s) de R$ 0.50\n", moedas/50);
+        moedas%=50;
+        System.out.printf("%d moeda(s) de R$ 0.25\n", moedas/25);
+        moedas%=25;
+        System.out.printf("%d moeda(s) de R$ 0.10\n", moedas/10);
+        moedas%=10;
+        System.out.printf("%d moeda(s) de R$ 0.05\n", moedas/5);
+        moedas%=5;
+        System.out.printf("%d moeda(s) de R$ 0.01\n", moedas);
     }
 }
