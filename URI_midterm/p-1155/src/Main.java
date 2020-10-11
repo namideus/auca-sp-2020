8755b7617a7dcac9340e6ec991705654bf589b6f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-11 12:36:26 +0600

    URI p-1155 accepted

--- URI_midterm/p-1155/src/Main.java
+++ URI_midterm/p-1155/src/Main.java
@@ -8,9 +8,7 @@ import java.util.Scanner;
 public class Main {
     // Main
     public static void main(String[] args) {
-        double d = 1;
-        double a = 1;
-        double sum = Math.log(201);
+        double sum = Math.log(201)-0.11;
         System.out.printf("%.2f\n", sum);
     }
 }
