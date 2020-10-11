39110b8c03152e4afc166287ead99f650599e828
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-11 12:55:48 +0600

    URI p-1159 wrong answer 5 %

---
+++ URI_midterm/p-1159/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x, sum;
+        while(true)
+        {
+            x = s.nextInt();
+            if(x==0) break;
+            sum = 0;
+            for(int t=0;t<5;t++)
+            {
+                if(x%2>0) ++x;
+                sum+=x;
+                x+=2;
+            }
+            System.out.printf("%d\n", sum);
+        }
+    }
+}
