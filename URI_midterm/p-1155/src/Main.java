1c8ac8687157b9f03ef880da64fed0e823c5c500
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-11 12:32:54 +0600

    URI p-1155 wrong answer

---
+++ URI_midterm/p-1155/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        double d = 1;
+        double a = 1;
+        double sum = Math.log(201);
+        System.out.printf("%.2f\n", sum);
+    }
+}
