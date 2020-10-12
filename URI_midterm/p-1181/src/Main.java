d902b59823be2c1cd3f6bad65f1fffe872caf1a2
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-12 11:06:44 +0600

    URI p-1081 accepted

---
+++ URI_midterm/p-1181/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @author  Yiman
+ *
+ * */
+
+public class Main {
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //double[][] arr = new double[12][12];
+        double d, sum = 0;
+        int n = s.nextInt();
+        String op = s.next();
+
+        for(int i=0; i<12; i++)
+        {
+            for(int j=0; j<12; j++)
+            {
+                d = s.nextDouble();
+                if(i==n) sum+=d;
+            }
+        }
+
+        if (op.equals("S"))
+            System.out.printf("%.1f\n", sum);
+        else
+            System.out.printf("%.1f\n", sum/12);
+
+        s.close();
+    }
+}
