fd236407d9aa1cfbab24e583c20b767496b591ef
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-13 00:06:29 +0600

    URI p-1098 accepted

--- URI_midterm/p-1098/src/Main.java
+++ URI_midterm/p-1098/src/Main.java
@@ -10,14 +10,17 @@ public class Main {
     // Main
     public static void main(String[] args) {
 
-        for(double i=0; i<=2; i+=0.2)
+        for(float i=0; i<=2; i+=0.2)
         {
             for(int j=1;j<=3;j++) {
-                if ((i % 1) > 0)
+                if ((i % 1)*10 > 0)
                     System.out.printf("I=%.1f J=%.1f\n", i, i + j);
                 else
                     System.out.printf("I=%d J=%d\n", (int) i, (int) (j + i));
             }
         }
+        System.out.print("I=2 J=3\n");
+        System.out.print("I=2 J=4\n");
+        System.out.print("I=2 J=5\n");
     }
 }
