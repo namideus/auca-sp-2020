ae5df93958538862c0e6af21e5ad2fa937fe5aed
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-21 14:48:02 +0600

    Task #3 solved

--- lab-04/src/com/auca/Problem03.java
+++ lab-04/src/com/auca/Problem03.java
@@ -1,8 +1,33 @@
 package com.auca;
 
+import java.util.Scanner;
+
 public class Problem03 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("month? ");
+        int month = s.nextInt();
+
+        switch(month)
+        {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("summer");
+                break;
+                default:
+                    System.out.println("autumn");
+        }
     }
 }
