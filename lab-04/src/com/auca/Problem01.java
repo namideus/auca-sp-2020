6be14f059ac8c7498ac14af727b53a0798201518
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-21 14:24:40 +0600

    Task #1 solved

--- lab-04/src/com/auca/Problem01.java
+++ lab-04/src/com/auca/Problem01.java
@@ -3,6 +3,14 @@ package com.auca;
 public class Problem01 {
 
     public static void main(String[] args) {
-        // write your code here
+        double sum = 0;
+        double d = 0.1;
+        for(int i=0;i<10;i++)
+            sum+=d;
+
+        if(Math.abs(sum-1)<1e-10)
+            System.out.print("Equal");
+        else
+            System.out.println("Not equal");
     }
 }
