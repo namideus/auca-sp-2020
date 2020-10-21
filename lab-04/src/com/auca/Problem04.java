1de991f9b01bc64ea6709fcfff67ffd1d8ea0f42
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-21 14:54:49 +0600

    Task #4 solved

--- lab-04/src/com/auca/Problem04.java
+++ lab-04/src/com/auca/Problem04.java
@@ -1,8 +1,20 @@
 package com.auca;
 
+import java.util.Scanner;
+
 public class Problem04 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print("complexity level? ");
+        int level = s.nextInt();
+        if(4<=level && level<=5)
+            System.out.println("You are a pro gamer.");
+        else if(2<=level && level<=3)
+            System.out.println("You are an experienced gamer.");
+        else if(level==1)
+            System.out.println("You are a beginner.");
+        else if(level==0)
+            System.out.println("You are a total newbie.");
     }
 }
