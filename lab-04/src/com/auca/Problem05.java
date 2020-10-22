6df7d31c04bb9218cb3eeb74d9814b07bb184855
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-22 15:56:12 +0600

    Task #5 lab-04 solved

--- lab-04/src/com/auca/Problem05.java
+++ lab-04/src/com/auca/Problem05.java
@@ -1,8 +1,30 @@
 package com.auca;
 
-public class Problem05 {
+import java.util.Scanner;
 
+public class Problem05 {
+    private static final int[] monthDays = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Year? ");
+        int year = s.nextInt();
+
+        System.out.print("Month? ");
+        int month = s.nextInt();
+        int days;
+
+        if(month==2 && isLeapYear(year))
+            days = monthDays[month-1]+1;
+        else
+            days = monthDays[month-1];
+
+        System.out.println("Number of days: "+days);
+    }
+
+    // Is leap year
+    private static boolean isLeapYear(int year)
+    {
+        return year%4==0 && year%100>0 || year%400==0;
     }
 }
