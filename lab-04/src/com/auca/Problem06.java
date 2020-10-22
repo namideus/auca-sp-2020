587656789a7cee42ed7fb02818a3c4288cdb39f0
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-10-22 16:49:40 +0600

    Task #6 lab-04 almost solved

--- lab-04/src/com/auca/Problem06.java
+++ lab-04/src/com/auca/Problem06.java
@@ -1,8 +1,19 @@
 package com.auca;
 
+import java.util.Scanner;
+
 public class Problem06 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a point with two coordinates: ");
+        double x = s.nextInt();
+        double y = s.nextInt();
+
+        if(x<=5 && y<=2.5 && x>=-5 && y<=2.5 && x>=-5 && y>=-2.5 && x<=5 && y>=-2.5)
+            System.out.printf("Point (%.1f, %.1f) is in the rectangle\n", x, y);
+        else
+            System.out.printf("Point (%.1f, %.1f) is not in the rectangle\n", x, y);
     }
 }
--- lab-04/src/com/auca/Problem07.java
+++ lab-04/src/com/auca/Problem07.java
@@ -1,8 +1,20 @@
 package com.auca;
 
+import java.util.Scanner;
+
 public class Problem07 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int lottery = (int)(Math.random()*1000)+100;
+
+        System.out.print("Enter 3-digit number: ");
+        int user = sc.nextInt();
+
+        if(lottery==user)
+            System.out.println("You won $10,000!");
+        else {
+
+        }
     }
 }
