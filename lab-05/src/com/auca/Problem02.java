1ea55615cf2303cd56a45ef02875afe9b3080442
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-18 18:52:57 +0600

    Problem#2 from lab-05 solved

--- lab-05/src/com/auca/Problem02.java
+++ lab-05/src/com/auca/Problem02.java
@@ -1,8 +1,21 @@
 package com.auca;
 
+import java.util.Scanner;
+
 public class Problem02 {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Integer? ");
+
+        int sum=0, n = scanner.nextInt();
+
+        while(n>0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+
+        System.out.println("The sum of all digits "+sum);
     }
 }
--- lab-05/src/com/auca/Problem04.java
+++ lab-05/src/com/auca/Problem04.java
@@ -1,8 +1,22 @@
 package com.auca;
 
-public class Problem04 {
+import processing.core.*;
+
+public class Problem04 extends PApplet {
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+       PApplet.main("Problem04");
     }
+
 }
