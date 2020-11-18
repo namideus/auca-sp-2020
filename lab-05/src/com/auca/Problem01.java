5ee52fb02004a88dfd0453704ed546d72cd0e79e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-18 15:24:54 +0600

    Created project-01 and lab-05 and solved Problem #1 from lab-05

---
+++ lab-05/src/com/auca/Problem01.java
@@ -0,0 +1,27 @@
+package com.auca;
+
+import java.util.Scanner;
+
+public class Problem01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, cnt = 0;
+        double sum = 0;
+
+        while(true)
+        {
+            n = scanner.nextInt();
+
+            if(n==0) break;
+
+            sum+=n;
+            ++cnt;
+        }
+
+        if(cnt==0)
+            System.out.println("Nothing to calculate");
+        else
+            System.out.printf("The arithmetic mean is %.1f\n", sum/cnt);
+    }
+}
---
+++ lab-05/src/com/auca/Problem04.java
@@ -0,0 +1,8 @@
+package com.auca;
+
+public class Problem04 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
