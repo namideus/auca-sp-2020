24d19b3bd5a4e9e0e53fc4456503433322e69d33
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-19 16:22:34 +0600

    Started problem-01 of project-01, created a ball at center

--- project-01/problem-01/src/Main.java
+++ project-01/problem-01/src/Main.java
@@ -1,2 +1,27 @@
-public class Main {
+import processing.core.*;
+
+public class Main extends PApplet {
+
+    private final int N = 100;
+    private final int MAX_R = 100;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        // noLoop();
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+
+        fill(100,10,100);
+        ellipse(width/2,height/2,MAX_R,MAX_R);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
 }
