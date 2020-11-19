3f921aeb4c4bc89223d7e41204c065b364dc124d
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-19 16:47:55 +0600

    problem-01 of project-01, realized basic vertical  movement of the ball

--- project-01/problem-01/src/Main.java
+++ project-01/problem-01/src/Main.java
@@ -5,22 +5,28 @@ public class Main extends PApplet {
     private final int N = 100;
     private final int MAX_R = 50;
     private float x;
+    private float y;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-        x = 0;
+        x = width/2f;
+        y = height-1;
     }
 
     public void draw() {
         background(0, 0, 0);
 
         fill(100,10,255);
-        ellipse(x,height/2f,MAX_R,MAX_R);
-        x+=10;
+        ellipse(x,y, MAX_R, MAX_R);
 
+//        if(x<width-(MAX_R/2f+10f))
+//            x+=10; // Horizontal
+
+        if(y>0+MAX_R/2f)
+            y-=10; // Vertical
     }
 
     public static void main(String[] args) {
