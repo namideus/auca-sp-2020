2093d253c02ec9f8796e3ae814520e98c6afba2c
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-23 19:43:10 +0600

    Problem #9 from lab-05 solved

--- lab-05/src/Problem09.java
+++ lab-05/src/Problem09.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Problem09 {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scan.nextLine();
+        boolean f = true;
+        for(int i=0, j=str.length()-1; i<j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j))
+            {
+                f = false;
+                break;
+            }
+        }
+        System.out.println(str+" "+((f)?"is a palindrome":"is not a palindrome"));
     }
 }
