4ab87c4fed3002b9011efb6d44c41a9eeb56d0c0
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-23 19:32:41 +0600

    Problem #5 from lab-05 solved

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -1,6 +1,31 @@
-public class Problem05 {
+import processing.core.*;
+
+public class Problem05 extends PApplet {
+
+    private final int N = 100;
+    private float alpha = 0;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        noLoop();
+        alpha = 90;
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+
+        for(int rad=0; rad<=900; rad+=50, alpha-=5)
+        {
+            fill(255f,50f,70f, alpha);
+            ellipse(width/2f, height/2f, rad, rad);
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        PApplet.main("Problem05");
     }
+
 }
