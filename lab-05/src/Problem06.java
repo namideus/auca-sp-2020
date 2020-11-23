be4b53ec8cc70e407924db1c5ebd03f20d0b6d29
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-23 19:37:42 +0600

    Problem #6 from lab-05 solved

--- lab-05/src/Problem06.java
+++ lab-05/src/Problem06.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Problem06 {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scan = new Scanner(System.in);
+	    int n, sum=0;
+
+	    while(true)
+        {
+            System.out.print("Enter an integer (the input ends if it is 0): ");
+            n = scan.nextInt();
+            if(n==0) break;
+            sum+=n;
+        }
+
+	    System.out.println("The sum is " + sum);
     }
 }
