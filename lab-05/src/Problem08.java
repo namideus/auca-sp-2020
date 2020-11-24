f6d22b1bed6d73a178dee763857d1fd1c4461c02
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-24 17:20:12 +0600

    Problem #8 from lab-05 solved

--- lab-05/src/Problem08.java
+++ lab-05/src/Problem08.java
@@ -1,6 +1,19 @@
 public class Problem08 {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("\t\t\tMultiplication Table\n\t\t");
+
+        for(int i=1; i<=9; i++)
+            System.out.print(i+"\t");
+
+        System.out.println("\n------------------------------------------");
+
+        for(int i=1; i<=9; i++)
+        {
+            System.out.print(i+"\t|\t");
+            for(int j=1; j<=9; j++)
+                System.out.print(i*j+"\t");
+            System.out.println();
+        }
     }
 }
