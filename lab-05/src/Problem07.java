cd113c71da6dc0e8fc626b28211b777018ad4085
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-11-26 15:51:53 +0600

    Problem #7 from lab-05 solved

--- lab-05/src/Problem07.java
+++ lab-05/src/Problem07.java
@@ -1,6 +1,31 @@
 public class Problem07 {
 
     public static void main(String[] args) {
-	// write your code here
+       int sum = 0;
+       int number = 0;
+
+       while(number<20)
+       {
+           sum+=++number;
+
+           if(sum>=100)
+               break;
+       }
+
+       System.out.printf("The number is %d\nThe sum is %d\n\n",number,sum);
+
+       number = 0;
+       sum = 0;
+       while(number<20)
+       {
+            ++number;
+
+            if(number==10 || number==11)
+                continue;
+
+            sum+=number;
+       }
+
+        System.out.printf("The sum is %d\n",sum);
     }
 }
