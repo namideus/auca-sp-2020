0702e44000485cbc7d1708e7dfac18108e12d186
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-01 16:15:43 +0600

    Solved Problem04 from lab-06

--- lab-06/src/Problem03.java
+++ lab-06/src/Problem03.java
@@ -1,6 +1,33 @@
-public class Problem03 {
+import processing.core.*;
+
+public class Problem03 extends PApplet {
+
+    private final int N = 100;
+    private final int MAX_R = 50;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        noLoop();
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+
+        drawStar(width/2f, height/2f,0, 100);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        PApplet.main("Problem03");
     }
+
+    public void drawStar(float x, float y, float r, float color)
+    {
+        fill(color, color, color);
+        line(x,y-50f,x,y+50f);
+       // line();
+    }
+
 }
