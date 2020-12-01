04e841292bcf3412436ca37d00fbc04c54eee638
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-01 15:53:58 +0600

    Solved Problem02.

--- lab-06/src/Problem02.java
+++ lab-06/src/Problem02.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Problem02 {
 
+    private static final int[] monthDays = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Year: ");
+        int year = scan.nextInt();
+
+        if(year<1)
+        {
+            System.out.println(year + " is not a correct year!");
+            System.exit(2);
+        }
+
+        System.out.print("Month: ");
+        int month = scan.nextInt();
+
+        if(month<1 || month>12)
+        {
+            System.out.println(month + " is not a correct month!");
+            System.exit(2);
+        }
+
+        // February and leap year?
+        int days = (month==2 && isLeapYear(year)) ? monthDays[month-1]+1 : monthDays[month-1];
+
+        System.out.println("Number of days: "+days);
+    }
+
+    // Is leap year
+    private static boolean isLeapYear(int year)
+    {
+        return year%4==0 && year%100>0 || year%400==0;
     }
 }
