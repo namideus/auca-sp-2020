04998f2ba27332c0a408920b80adc180d7f44c92
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-02 20:28:54 +0600

    Solved Problem09 from lab-06

--- lab-06/src/Problem03.java
+++ lab-06/src/Problem03.java
@@ -16,18 +16,31 @@ public class Problem03 extends PApplet {
     public void draw() {
         background(0, 0, 0);
 
-        drawStar(width/2f, height/2f,0, 100);
+       // drawStar(width/2f, height/2f,0, 100);
     }
 
     public static void main(String[] args) {
         PApplet.main("Problem03");
     }
 
-    public void drawStar(float x, float y, float r, float color)
+    public void drawStar()
     {
-        fill(color, color, color);
-        line(x,y-50f,x,y+50f);
+       //  fill(color, color, color);
+       // line(x,y-50f,x,y+50f);
        // line();
+
+        float r = min(width, height)/3f;
+        float x = width/2f;
+        float y = height/2f;
+        float red = 255;
+        float green = 0;
+        float blue = 0;
+
+        float x1 = x-r;
+        float y1 = y;
+
+        float x2 = x+r;
+
     }
 
 }
--- lab-06/src/Problem09.java
+++ lab-06/src/Problem09.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Problem09 {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("i\t\t\t\t\t\tm(i)\n------------------------------\n");
+
+        for(long i=1; i<=901; i+=100)
+            System.out.printf("%d\t\t\t\t\t\t%.4f\n", i, m(i));
+
+    }
+
+    // Estimate PI
+    private static double m(long i)
+    {
+        double sum = 0;
+
+        for(int j=1; j<=i; j++)
+            sum += Math.pow(-1,j+1)/(2*j-1);
+
+        return sum*4;
     }
 }
