c1ad3bc36c4d76d9ed7211062795c8073b2abd60
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-02 13:22:45 +0600

    Solved Problem07 from lab-06

--- lab-06/src/Problem07.java
+++ lab-06/src/Problem07.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Problem07 {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        long number = scan.nextLong();
+
+        System.out.printf("Reverse of %d is %d\n", number, reverse(number));
+        System.out.println(is_palindrome(number)? "Palindrome!" : "Not palindrome!" );
     }
+
+    public static long reverse(long number)
+    {
+        long reversed = 0;
+
+        while(number>0)
+        {
+            reversed = reversed*10 + number%10;
+            number/=10;
+        }
+
+        return reversed;
+    }
+
+    public static boolean is_palindrome(long number)
+    {
+        return number == reverse(number);
+    }
+
 }
