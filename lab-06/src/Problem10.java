df5bc8ea9510857ca12442a783e6f4d3be9415dd
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-02 20:32:41 +0600

    Solved Problem10 from lab-06

--- lab-06/src/Problem10.java
+++ lab-06/src/Problem10.java
@@ -1,6 +1,22 @@
 public class Problem10 {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        System.out.print("i\t\t\t\t\t\tm(i)\n------------------------------\n");
+
+        for(long i=1; i<=20; i++)
+            System.out.printf("%d\t\t\t\t\t\t%.4f\n", i, m(i));
+
+    }
+
+    // Sum series
+    private static double m(long i)
+    {
+        double sum = 0;
+
+        for(float j=1; j<=i; j++)
+            sum += j/(j+1);
+
+        return sum;
     }
 }
