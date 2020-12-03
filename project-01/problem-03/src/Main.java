3fce5a8fb05f875afa6d9f5b70343aa110d3ec04
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-03 16:24:34 +0600

    problem-03 of project-01: enabled animation of texts on screen

--- project-01/problem-03/src/Main.java
+++ project-01/problem-03/src/Main.java
@@ -15,20 +15,37 @@ public class Main extends PApplet {
     public void setup() {
         frameRate(20f);
 
-        firstText = JOptionPane.showInputDialog("Enter the first text:");
-        secondText = JOptionPane.showInputDialog("Enter the second text:");
+        x1 = width/2f;
+        x2 = width/2f;
+
+        firstText = JOptionPane.showInputDialog("Enter the first text:").trim();
+        secondText = JOptionPane.showInputDialog("Enter the second text:").trim();
     }
 
     public void draw() {
         background(0, 0, 0);
 
+        if(f) {
+            x1-=5;
+            x2+=5;
+
+            if(x1==20)
+                f = false;
+        } else {
+            x1+=5;
+            x2-=5;
+
+            if(x1==width-20)
+                f = true;
+        }
+
         fill(255, 0, 0);
         textSize(100f);
-        text(firstText, width / 2f, height / 2.5f);
+        text(firstText, x1, height / 2.5f);
 
         fill(255, 255, 0);
         textSize(100f);
-        text(secondText, width / 2f, height / 1.5f);
+        text(secondText, x2, height / 1.5f);
     }
 
     public static void main(String[] args) {
