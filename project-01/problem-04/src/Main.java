614367c4bf71e455f08c9cd2544ef7f79c0fc22b
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-05 19:50:30 +0600

    problem-04 of project-01: tried to create a grid

--- project-01/problem-04/src/Main.java
+++ project-01/problem-04/src/Main.java
@@ -6,18 +6,34 @@ import java.awt.*;
 
 public class Main extends PApplet {
 
+    private int fielSize;
+
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
         frameRate(30f);
+
+       // fielSize = Integer.getInteger(JOptionPane.showInputDialog("Enter the field's size [10,40]:").trim());
     }
 
     public void draw() {
         background(0, 0, 0);
 
+        drawGrid(width/2f,height/2f);
 
+
+//        for(int i=0; i<fielSize; i++)
+//            for(int j=0; j<fielSize; j++)
+//            {
+//
+//            }
+    }
+
+    public void drawGrid(float x, float y)
+    {
+        line(x,10f,x,height-10f);
     }
 
     public static void main(String[] args) {
