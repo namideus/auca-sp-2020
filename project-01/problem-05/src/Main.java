fb3775f0c520ca30a84efea62eb19218c9cf0656
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-08 21:41:40 +0600

    problem-05 of project-01: trying to draw grid (board)

--- project-01/problem-05/src/Main.java
+++ project-01/problem-05/src/Main.java
@@ -1,2 +1,48 @@
-public class Main {
+import processing.core.*;
+
+public class Main extends PApplet {
+
+    private float x,y, dx,dy,columns,rows;
+    private String text;
+    private float D = 50;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        columns = min(height,width)/D;
+        rows = min(height,width)/D;
+    }
+
+    public void draw() {
+        background(0,0,0);
+
+        for(int y=0; y<width; y+=D) {
+            for (int x = 0; x <width; x+=D) {
+                if((x+y)%2!=0)
+                    fill(255,0,0);
+                else
+                    fill(0,0,255);
+                stroke(0);
+                rect(x, y , D, D);
+            }
+        }
+    }
+
+//        for(float y=0; y<rows; y+=D) {
+//            for (float x = 0; x<columns; x+=D) {
+//                if((x+y)%2!=0)
+//                    fill(255);
+//                else
+//                    fill(0);
+//                stroke(0);
+//                rect(x, y , D, D);
+//            }
+//        }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
 }
