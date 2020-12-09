a0dcb34c56d23568d65e33f30b96de990d14869f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-09 22:46:34 +0600

    Problem08 of lab-07 solved

--- lab-07/src/Problem08.java
+++ lab-07/src/Problem08.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem08 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int n, below=0, above=0;
+        double sum = 0, average;
+        System.out.println("Enter scores (negative to stop): ");
+        while(true)
+        {
+            n = scanner.nextInt();
+            if(n<0)
+                break;
+            sum += n;
+            arrayList.add(n);
+        }
+        average = sum/arrayList.size();
+        for(int score: arrayList)
+        {
+            if(score>=average)
+                ++above;
+        }
+        System.out.printf("Scores >= average: %d\nScores < average: %d\n",
+                above, arrayList.size()-below);
     }
 }
