49e6f59878f51d5f2cfa673cb5537c7953ed4dd7
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-11 23:23:39 +0600

    Problem02 of lab-08 solved

--- lab-08/src/Problem02.java
+++ lab-08/src/Problem02.java
@@ -3,25 +3,35 @@ import java.util.Scanner;
 public class Problem02 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 3 rows and 4 columns: ");
+        int correctCount;
+        char[][] answers = {
+                {'A','B','C','D','E','B','B','D','D','C'},
+                {'B','C','C','D','A','B','D','D','A','A'},
+                {'A','D','A','A','E','B','B','D','D','C'},
+                {'C','B','C','A','A','B','D','E','E','D'},
+                {'D','B','C','D','E','B','B','D','D','C'},
+                {'A','A','A','A','E','B','A','A','C','A'},
+                {'C','C','D','A','B','A','A','A','C','D'},
+                {'A','B','B','B','B','B','B','D','D','C'}
+        };
 
-        int[][] arr = new int[3][4];
+        char[] keys =   {'A','A','C','D','E','A','B','E','B','C'};
+        int[] correctAnswers = new int[answers.length];
 
-        for(int i=0; i<3;i++)
-            for(int j=0;j<4;j++)
-                arr[i][j] = scan.nextInt();
+        for(int i=0; i<answers.length; i++)
+        {
+            correctCount = 0;
 
-        System.out.println("Sum of all elements: "+getSum(arr));
-    }
+            for(int j=0; j<answers[i].length; j++)
+            {
+                if (answers[i][j]==keys[j])
+                    correctCount++;
+            }
 
-    public static int getSum(int[][] arr)
-    {
-        int sum = 0;
-        for(int i=0; i<3;i++)
-            for(int j=0;j<4;j++)
-                sum+=arr[i][j];
+            correctAnswers[i] = correctCount;
+        }
 
-        return sum;
+        for(int i=0; i<correctAnswers.length; i++)
+            System.out.printf("Student %d's correct count is %d\n", i,correctAnswers[i]);
     }
 }
