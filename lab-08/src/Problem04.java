3d3fd28734adcf54d11b86d7b4bf7ef3521a2654
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-12 22:05:46 +0600

    Problem04 of lab-08 solved

--- lab-08/src/Problem04.java
+++ lab-08/src/Problem04.java
@@ -4,24 +4,52 @@ public class Problem04 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 3 rows and 4 columns: ");
+        System.out.println("Enter a Sudoku puzzle solution: ");
 
-        int[][] arr = new int[3][4];
+        boolean flag = true;
+        int k;
+        int[][] matrix = new int[9][9];
+        int[] sl = new int[9];
+        int[] sc = new int[9];
+        int[][] sm = new int[3][3];
 
-        for(int i=0; i<3;i++)
-            for(int j=0;j<4;j++)
-                arr[i][j] = scan.nextInt();
+        for(int i=0;i<9;i++) {
+            for (int j = 0; j < 9; j++) {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
 
-        System.out.println("Sum of all elements: "+getSum(arr));
-    }
+        for(int i=0;i<9;i++) {
+            for (int j = 0; j < 9; j++) {
+                k = matrix[i][j];
+                k*=k;
+                sl[i] += k;
+                sc[j] += k;
+                sm[i/3][j/3] += k;
 
-    public static int getSum(int[][] arr)
-    {
-        int sum = 0;
-        for(int i=0; i<3;i++)
-            for(int j=0;j<4;j++)
-                sum+=arr[i][j];
+                if(i==8 && sc[j]!=285) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(sl[i]!=285) {
+                flag = false;
+                break;
+            }
+        }
 
-        return sum;
+        if(flag)
+        {
+            for(int i=0;i<3;i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (sm[i][j] != 285) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(((flag)?"Valid " : "Invalid ")+"solution");
     }
 }
