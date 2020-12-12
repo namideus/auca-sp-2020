9b04348144e284c9fb5892e1fe7aaaab891ba53f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-12 22:08:59 +0600

    Problem05 of lab-07 selection sort implemented

--- lab-07/src/Problem05.java
+++ lab-07/src/Problem05.java
@@ -1,9 +1,11 @@
 public class Problem05 {
     public static void main(String[] args) {
-
+        int[] unsortedArray = {9,5,6,2,5,9,1,5,6,3,8};
+        int[] sorted = selectionSort(unsortedArray);
+        for (int i : sorted) System.out.println(i);
     }
 
-    public static void selectionSort(int[] list)
+    public static int[] selectionSort(int[] list)
     {
         for(int i=0; i<list.length-1;i++)
         {
@@ -24,5 +26,7 @@ public class Problem05 {
                 list[i] = min;
             }
         }
+
+        return list;
     }
 }
