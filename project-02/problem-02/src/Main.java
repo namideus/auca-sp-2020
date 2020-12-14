be87a09d2cbab951df46f323505660bb3c0af16e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-14 15:26:53 +0600

    problem02 of project-02: almost ready, only requires the design of snowflakes

--- project-02/problem-02/src/Main.java
+++ project-02/problem-02/src/Main.java
@@ -5,13 +5,9 @@ import javax.swing.*;
 public class Main extends PApplet {
 
     private int flakeNumber;
-    PShape[] flake_shapes;
-
-    float[] x;
-    float[] y;
-    float[] size;
-    float[] spin;
-    float[] spinspeed;
+    private float[] coordsX;
+    private float[] coordsY;
+    private Snowflake[] flakes;
 
     public void settings() {
         fullScreen();
@@ -20,34 +16,70 @@ public class Main extends PApplet {
     public void setup() {
         flakeNumber = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of flakes:").trim());
 
-        x = new float[flakeNumber];
-        y = new float[flakeNumber];
-
-        for(int i=0; i<flakeNumber; i++) {
-            x[i] = random(0, width - 1);
-            y[i] = random(0, height - 1);
-        }
+        flakes = new Snowflake[flakeNumber];
+        for(int i=0; i<flakeNumber; i++)
+            flakes[i] = new Snowflake();
     }
 
     public void draw() {
         background(0, 0, 0);
 
-//        for(int i=0; i<flakeNumber; i++) {
-//            flakes[i].show();
-//            flakes[i].fall();
-//        }
+        for(int i=0; i<flakeNumber; i++) {
+            flakes[i].show();
+            flakes[i].fall();
+        }
     }
 
     public static void main(String[] args) {
         PApplet.main("Main");
     }
 
-    void drawFlakes()
-    {
-        for(int i=0; i<12; i++)
+    private class Snowflake {
+        float x = random(width);
+        float y = random(-200,-100);
+        float r = random(5,10);
+        float angle = random(0,360);
+        float yspeed = random(5,11);
+        float rspeed = random(3,6);
+
+        void fall() {
+                y += yspeed;
+
+                if (y > height)
+                    y = random(-200, -100);
+        }
+
+        void rotate()
         {
-            pushMatrix();
-            translate(x[i], y[i]);
+            angle += rspeed;
+        }
+
+        void show()
+        {
+            PShape s = createShape();
+            s.beginShape();
+            s.fill(0, 0, 255);
+            s.noStroke();
+            s.vertex(0, 0);
+            s.vertex(0, 50);
+            s.vertex(50, 50);
+            s.vertex(50, 0);
+            s.rotate(angle);
+            s.endShape(CLOSE);
+            shape(s,x,y);
+
+//            strokeWeight(4f);
+//            stroke(255,255,255);
+//
+//            float y1 = y-r;
+//            float y2 = y+r;
+//            line(x,y1,x,y2);
+//            line(x-30f,y1,x+30f,y2);
+//            line(x+30f,y1,x-30f,y2);
+//
+//            float x3 = x-r;
+//            float x4 = x+r;
+//            line(x3,y,x4,y);
         }
     }
 }
