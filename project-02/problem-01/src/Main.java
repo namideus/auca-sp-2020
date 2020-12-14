57eada34f764ce74e8b4179bf1f44ac032f71659
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-14 12:33:19 +0600

    Problem01 of project-02: created a single snowflake

--- project-02/problem-01/src/Main.java
+++ project-02/problem-01/src/Main.java
@@ -9,15 +9,11 @@ public class Main extends PApplet {
 
     public void settings() {
         fullScreen();
-        // size(200, 200, P2D);
     }
 
-   /* public void setup() {
+    public void setup() {
         starNumber = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of stars:").trim());
 
-    }*/
-
-    public void setup() {
     }
 
     public void draw() {
@@ -32,37 +28,20 @@ public class Main extends PApplet {
 
     public void drawStar(float x, float y, float r, float blue, float green, float red)
     {
-        strokeWeight(3f);
+        strokeWeight(4f);
         stroke(red,green,blue);
 
         float y1 = y-r;
         float y2 = y+r;
-        line(x,y1,x,y2);
+        line(x,y1-7f,x,y2+7f);
+        line(x-30f,y1,x+30f,y2);
+        line(x+30f,y1,x-30f,y2);
 
         float x3 = x-r;
         float x4 = x+r;
-        line(x3,y,x4,y);
-
-        float x5 = x + r/1.5f;
-        float y5 = y - r/1.5f;
-        float x6 = x - r/1.5f;
-        float y6 = y + r/1.5f;
-        line(x5,y5,x6,y6);
-
-        float x7 = x + r/1.5f;
-        float y7 = y + r/1.5f;
-        float x8 = x - r/1.5f;
-        float y8 = y - r/1.5f;
-        line(x7,y7,x8,y8);
-
-//        line(x5,y5,x,y1);
-//        line(x8,y8,x,y1);
-//        line(x3,y,x8,y8);
-//        line(x3,y,x6,y6);
-//        line(x,y2,x6,y6);
-//        line(x,y2,x7,y7);
-//        line(x4,y,x7,y7);
-//        line(x4,y,x5,y5);
+        line(x3-7f,y,x4+7f,y);
+        line(x3,y-30f,x4,y+30f);
+        line(x3,y+30f,x4,y-30f);
     }
 
 /*    public void animateCircle3()
