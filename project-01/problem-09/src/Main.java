b08609a9ff2021ecc443be60bb63f4eddb58e727
Altynbek uulu Yiman <altynbek_y@auca.kg>
2020-12-18 13:02:27 +0600

    problem-09 of project-01: realized collision of the rectangle

--- project-01/problem-09/src/Main.java
+++ project-01/problem-09/src/Main.java
@@ -2,6 +2,9 @@ import processing.core.*;
 
 public class Main extends PApplet {
 
+    private final int MAX_EXTEND = 100;
+    private float x1,y1;
+    private boolean fx,fy;
     private float angle = 0;
     private float dangle = .1f;
 
@@ -11,22 +14,51 @@ public class Main extends PApplet {
 
     public void setup() {
         noStroke();
+        frameRate(30f);
+
+        x1 = width/2f;
+        y1 = height/2f;
     }
 
     public void draw() {
-        fill(0,0,0,20);
+        fill(0,0,0,60);
         rect(0,0,width-1,height-1);
 
-        translate(width*.5f,height*.5f);
-        rotate(angle);
-        fill(255);
-        rect(-50,-50,100,100);
-
-        angle+=dangle;
+        animateRects();
     }
 
     public static void main(String[] args) {
         PApplet.main("Main");
+    }
+
+    public void animateRects()
+    {
+        translate(x1,y1);
+        rotate(angle);
+        fill(255);
+        rect(-50,-50,100,100);
+
+        if(fx) {
+            x1-=10;
+            if(x1<=MAX_EXTEND)
+                fx = false;
+        } else {
+            x1+=10;
+            if(x1>=width-MAX_EXTEND)
+                fx = true;
+        }
+
+        if(fy) {
+            y1-=10;
+            if(y1<=MAX_EXTEND)
+                fy = false;
+        } else {
+            y1+=10;
+            if(y1>=height-MAX_EXTEND)
+                fy = true;
+        }
+
+        angle+=dangle;
     }
 
 }
